d1edc2c1a095bba53b0148e210713f9a27a5f17d
SyedOrnab <73894699+SyedOrnab@users.noreply.github.com>
2021-08-16 23:20:30 +0600

    Delete Final Project directory

--- Final Project/Finalvacreport.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include 'mainheader.php';
-?>
-    <p align="center">Thank you <?php echo $_SESSION["loggeddoc"]; ?> for submitting your vaccine Information</p>
-    <p align="center"><a href="vacreportDetails.php">You can show all doctor's vaccine report</a></p>
-<?php include'mainfooter.php'; ?>
